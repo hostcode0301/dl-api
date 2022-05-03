341c29c777abbd3197149c48fcfdd9bb0fa5d170
Thiện Phú Trần <hostcode0301@gmail.com>
2022-05-03 20:30:25 +0700

    Try reading Config Var from heroku

--- dl_api/dependencies.py
+++ dl_api/dependencies.py
@@ -1,3 +1,4 @@
+import os
 from .dl.resnet_utils import CatDogModel
 
 # Init model for detect
@@ -6,3 +7,6 @@ dl_model = CatDogModel()
 
 async def init_model():
     return dl_model
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+print("Config var: AWS_ACCESS_KEY_ID = ", AWS_ACCESS_KEY_ID)
